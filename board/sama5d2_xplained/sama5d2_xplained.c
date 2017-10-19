beed99b72dc4d75115d819b3646042eb02e29528
Martin Zvak <martin.zvak@honeywell.com>
2017-10-19 16:54:53 +0200

    Test commit to remotes

--- board/sama5d2_xplained/sama5d2_xplained.c
+++ board/sama5d2_xplained/sama5d2_xplained.c
@@ -25,6 +25,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+#include "test.h"
 #include "common.h"
 #include "hardware.h"
 #include "pmc.h"
